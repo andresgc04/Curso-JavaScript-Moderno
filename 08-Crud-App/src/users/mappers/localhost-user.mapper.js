b900d08189e951af428095b81c147f1e050dcabd
andresgc04 <andresgc1997@outlook.com>
2026-01-10 17:34:47 -0400

    add
    Mappers

---
+++ 08-Crud-App/src/users/mappers/localhost-user.mapper.js
@@ -0,0 +1,22 @@
+import { User } from "../models/user";
+
+/**
+ *
+ * @param {Like<User>} localHostUser
+ * @returns {User}
+ */
+
+export const localhostUserToModel = (localHostUser) => {
+  const { avatar, balance, first_name, gender, id, isActive, last_name } =
+    localHostUser;
+
+  return new User({
+    avatar,
+    balance,
+    firstName: first_name,
+    gender,
+    id,
+    isActive,
+    lastName: last_name,
+  });
+};
