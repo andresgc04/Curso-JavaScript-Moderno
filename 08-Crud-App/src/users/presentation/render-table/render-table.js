186cb89e20d7fde122048ae9a4f1828cccb037f0
andresgc04 <andresgc1997@outlook.com>
2026-01-17 18:03:10 -0400

    add
    Agregar funcionalidad de cargar informacion del usuario al modal

--- 08-Crud-App/src/users/presentation/render-table/render-table.js
+++ 08-Crud-App/src/users/presentation/render-table/render-table.js
@@ -1,4 +1,5 @@
 import usersStore from "../../store/users-store";
+import { showModal } from "../render-modal/render-modal";
 import "./render-table.css";
 
 let table;
@@ -24,6 +25,18 @@ const createTable = () => {
 
 /**
  *
+ * @param {PointerEvent} event
+ */
+const tableSelectListener = (event) => {
+  const element = event.target.closest(".select-user");
+  if (!element) return;
+
+  const id = element.getAttribute("data-id");
+  showModal(id);
+};
+
+/**
+ *
  * @param {HTMLDivElement} element
  */
 export const renderTable = (element) => {
@@ -34,6 +47,7 @@ export const renderTable = (element) => {
     element.append(table);
 
     //TODO: listeners a la table
+    table.addEventListener("click", tableSelectListener);
   }
 
   let tableHTML = "";
@@ -47,9 +61,9 @@ export const renderTable = (element) => {
             <td>${user.lastName}</td>
             <td>${user.isActive}</td>
             <td>
-                <a href="#/" data-id="${user.id}">Select</a>
+                <a href="#/" class="select-user" data-id="${user.id}">Select</a>
                 |
-                <a href="#/" data-id="${user.id}">Delete</a>
+                <a href="#/" class="delete-user" data-id="${user.id}">Delete</a>
             </td>
         </tr>
     `;
